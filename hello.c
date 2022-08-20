77cdeb51a163e8639a63b3ec388ae3eff6ff85d9
Kpatdu <111658815+Kpatdu@users.noreply.github.com>
2022-08-20 21:27:40 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,1 +1,17 @@
-Kristian Patdu
+/**
+ * Author: Kristian Patdu
+ *         kpatdu2@huskers.unl.edu
+ * Date: 2022/08/20
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+printf("Kristian Patdu!\n");
+printf("Computer Engineering\n");
+return 0;
+}
